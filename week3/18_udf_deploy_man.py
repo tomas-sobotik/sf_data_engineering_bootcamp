a4f804ef0c0ebe50f6d9b68a2085338645b6d7fa
Tomas Sobotik <52287069+tomas-sobotik@users.noreply.github.com>
2023-04-21 12:42:34 +0200

    Add files via upload

---
+++ week3/18_udf_deploy_man.py
@@ -0,0 +1,36 @@
+#Step 1: Define the function for the UDF
+
+def double(input_value: int):
+  return input_value*2
+
+
+#Step 2: we need to import data type for returned value
+from snowflake.snowpark.types import IntegerType
+
+#Step 3:  Register UDF in Snowflake and Upload UDF to Snowflake
+from snowflake.snowpark import Session
+
+connection_parameters = {
+    "account": "BV72174.eu-west-1",
+    "user": "sobottom",
+    "password": "TC*h9*fNa!aexxQ",
+   "role": "SYSADMIN",  # optional
+   "warehouse": "COMPUTE_WH",  # optional
+    "database": "DATA_ENGINEERING",  # optional
+    "schema": "PUBLIC",  # optional
+} 
+
+snowpark_session = Session.builder.configs(connection_parameters).create() 
+
+snowpark_session.udf.register(
+    func = double
+  , return_type = IntegerType()
+  , input_types = [IntegerType()]
+  , is_permanent = True
+  , name = 'SNOWPARK_DOUBLE'
+  , replace = True
+  , stage_location = '@UDF_STAGE'
+)
+
+#Step 4: Test the UDF by calling 
+snowpark_session.sql('SELECT SNOWPARK_DOUBLE(2)').show()
